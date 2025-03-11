432056d1a51c33cfe76b1e4e3c2c0acf4c329dab
楊中 <wherechi0912@gmail.com>
2025-03-11 12:14:24 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -3,7 +3,6 @@ import numpy as np
 import pickle
 import random
 import gym
-
 def get_action(obs):
     
     # TODO: Train your own agent
@@ -11,8 +10,12 @@ def get_action(obs):
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
+    # with open("q_table.pkl", "rb") as f:
+    #     Q_table = pickle.load(f)
 
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    try:
+        return np.argmax(Q_table[obs])
+    except:
+        return random.choice([0, 1, 2, 3, 4, 5])
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
