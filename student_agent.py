267b6581924f770ccbfc34054f32d495382290e9
楊中 <wherechi0912@gmail.com>
2025-03-21 13:45:18 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -5,14 +5,15 @@ import random
 import gym
 
 q_table = None
-with open("q_table.pkl", "rb") as f:
+with open("q_table_1.pkl", "rb") as f:
     q_table = pickle.load(f)
+    print (len(q_table))
 visited = []
 has_picked_up = False
 target_loc = None
 destination = None
 
-def get_state(obs, target_loc=None):
+def get_state(obs, target_loc=None, has_picked_up=False):
 	stations = [[0, 0], [0, 4], [4, 0], [4,4]]
 	taxi_row, taxi_col, stations[0][0],stations[0][1] ,stations[1][0],stations[1][1],stations[2][0],stations[2][1],stations[3][0],stations[3][1],obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
 	stations = [tuple(i) for i in stations]	
@@ -22,8 +23,8 @@ def get_state(obs, target_loc=None):
 	y_dir = target_loc[1] - taxi_col
 	x_dir = 0 if x_dir == 0 else x_dir // abs(x_dir)
 	y_dir = 0 if y_dir == 0 else y_dir // abs(y_dir)
-	# return (x_dir, y_dir, obstacle_north, obstacle_south, obstacle_east, obstacle_west, has_picked_up)
-	return (obstacle_north, obstacle_south, obstacle_east, obstacle_west)
+	return (x_dir, y_dir, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look, has_picked_up)
+	# return (obstacle_north, obstacle_south, obstacle_east, obstacle_west)
   
 def get_action(obs):
 	"""
@@ -42,7 +43,7 @@ def get_action(obs):
 	taxi_row, taxi_col, stations[0][0],stations[0][1] ,stations[1][0],stations[1][1],stations[2][0],stations[2][1],stations[3][0],stations[3][1],obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
  
    
-	if is_in_station(obs) and (taxi_row, taxi_col) not in visited:
+	if is_in_station(obs):
 			visited.append((taxi_row, taxi_col))
 			if destination_look:
 				destination = (taxi_row, taxi_col)
@@ -56,46 +57,20 @@ def get_action(obs):
 						break	
 				# print (visited, (taxi_row, taxi_col), target_loc)
   
-	if get_state(obs, target_loc) not in q_table:
+	if get_state(obs, target_loc, has_picked_up) not in q_table:
+			# print(get_state(obs, target_loc, has_picked_up), destination)
 			action_probs = np.ones(6) / 6
-			# if obstacle_south:
-			# 	action_probs[0] = 0
-			# if obstacle_north:
-			# 	action_probs[1] = 0
-			# if obstacle_east:
-			# 	action_probs[2] = 0
-			# if obstacle_west:
-			# 	action_probs[3] = 0	
-			# if not passenger_look or has_picked_up or not is_in_station(obs):
-			# 	action_probs[4] = 0
-			# if not destination_look or not has_picked_up or not is_in_station(obs):
-			# 	action_probs[5] = 0
-			# action_probs = action_probs / np.sum(action_probs)
 			action = np.random.choice(6, p=action_probs)  # Random action
+			q_table[get_state(obs, target_loc, has_picked_up)] = action_probs
 	else:
-			action = np.argmax(q_table[get_state(obs, target_loc)])  # Greedy action
+			action = np.argmax(q_table[get_state(obs, target_loc, has_picked_up)])  # Greedy action
 	
+	# print (action, q_table[get_state(obs, target_loc, has_picked_up)])
+ 
 	if not has_picked_up and passenger_look and is_in_station(obs) and action == 4:
 			has_picked_up = True
 	if has_picked_up and destination_look and is_in_station(obs) and action == 5:
 			done = True
-	
-	# action_probs = np.ones(6) / 6
-	# if obstacle_south:
-	# 	action_probs[0] = 0
-	# if obstacle_north:
-	# 	action_probs[1] = 0
-	# if obstacle_east:
-	# 	action_probs[2] = 0
-	# if obstacle_west:
-	# 	action_probs[3] = 0	
-	# if not passenger_look or has_picked_up or not is_in_station(obs):
-	# 	action_probs[4] = 0
-	# if not destination_look or not has_picked_up or not is_in_station(obs):
-	# 	action_probs[5] = 0
-	# action_probs = action_probs / np.sum(action_probs)
-	# action = np.random.choice(6, p=action_probs)  # Random action
-	# print(get_state(obs, target_loc), q_table[get_state(obs, target_loc)])
 	return action
 
 def is_in_station(obs):
